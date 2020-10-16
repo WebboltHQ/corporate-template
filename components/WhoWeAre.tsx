1a60e3c282e33f1ec431d073bb5676845518e71a
taishikato <taishi.k0903@gmail.com>
2020-10-16 10:59:14 -0700

    fix team

--- components/WhoWeAre.tsx
+++ components/WhoWeAre.tsx
@@ -8,14 +8,14 @@ const WhoWeAre = ({ team }) => {
     <section className={`${classes.wrapper} text-gray-700 body-font`}>
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-col text-center w-full mb-20">
-          <H2>Our Team</H2>
+          <H2>{team.title}</H2>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
             Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.
             Franzen you probably haven't heard of them.
           </p>
         </div>
         <div className="flex flex-wrap -m-4">
-          {team.map(member => (
+          {team.member.map(person => (
             <div className="p-4 lg:w-1/2">
               <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
                 <img
@@ -24,9 +24,9 @@ const WhoWeAre = ({ team }) => {
                   src="https://dummyimage.com/200x200"
                 />
                 <div className="flex-grow sm:pl-8">
-                  <h2 className="title-font font-medium text-lg text-gray-900">{member.name}</h2>
-                  <h3 className="text-gray-500 mb-3">{member.jobTitle}</h3>
-                  <p className="mb-4">{member.description}</p>
+                  <h2 className="title-font font-medium text-lg text-gray-900">{person.name}</h2>
+                  <h3 className="text-gray-500 mb-3">{person.jobTitle}</h3>
+                  <p className="mb-4">{person.description}</p>
                   <span className="inline-flex">
                     <a className="text-gray-500">
                       <svg
